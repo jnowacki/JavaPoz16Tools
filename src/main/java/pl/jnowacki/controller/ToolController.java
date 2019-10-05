d300094dc78b22c776ec0878b595c92bb13e7380
Jedrzej Nowacki <contact@jnowacki.pl>
2019-10-05 13:54:35 +0200

    Tool service impl

---
+++ src/main/java/pl/jnowacki/controller/ToolController.java
@@ -0,0 +1,7 @@
+package pl.jnowacki.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+public class ToolController extends HttpServlet {
+}
--- src/main/java/pl/jnowacki/service/ToolServiceImpl.java
+++ src/main/java/pl/jnowacki/service/ToolServiceImpl.java
@@ -1,12 +1,17 @@
 package pl.jnowacki.service;
 
+import pl.jnowacki.dao.ToolDao;
+import pl.jnowacki.dao.ToolDaoImpl;
 import pl.jnowacki.model.Tool;
 
 import java.util.List;
 
 public class ToolServiceImpl implements ToolService {
+
+    private ToolDao toolDao = new ToolDaoImpl();
+
     @Override
     public List<Tool> getAll() {
-        return null;
+        return toolDao.getAll();
     }
 }
