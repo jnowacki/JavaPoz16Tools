193842d3aa263d2fc7924189bdbdb09013e82878
Jedrzej Nowacki <contact@jnowacki.pl>
2019-10-06 11:03:02 +0200

    Login POST

--- src/main/java/pl/jnowacki/controller/LoginController.java
+++ src/main/java/pl/jnowacki/controller/LoginController.java
@@ -1,5 +1,7 @@
 package pl.jnowacki.controller;
 
+import org.apache.commons.lang3.StringUtils;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,5 +16,17 @@ public class LoginController extends HttpServlet {
         getServletContext().getRequestDispatcher("/login.jsp").forward(req, resp);
     }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userName = req.getParameter("username");
+        String password = req.getParameter("password");
 
+        if (StringUtils.isNotEmpty(userName.trim()) && StringUtils.isNotEmpty(password.trim())){
+            req.getSession().setAttribute("username", userName);
+            resp.sendRedirect(getServletContext().getContextPath());
+        } else {
+
+            getServletContext().getRequestDispatcher("/login.jsp").forward(req, resp);
+        }
+    }
 }
